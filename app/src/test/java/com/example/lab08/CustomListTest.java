46d82f018998825ca59034670575bcc00c7651bd
Dotun Famobio <dfamobio@ualberta.ca>
2025-10-31 02:53:21 -0600

    failing delete city test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class CustomListTest {
     @Test
     public void testHasCity() {
@@ -11,5 +12,17 @@ public class CustomListTest {
 // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+
+        assertFalse(list.hasCity(calgary));
+    }
+
 
 }
